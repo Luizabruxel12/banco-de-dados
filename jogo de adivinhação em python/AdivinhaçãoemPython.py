95d39d2ae69537e152e8d12f71d2964552d4a0c5
Luizabruxel12 <luiza.bruxel@escola.pr.gov.br>
2026-02-27 12:25:07 -0300

    Add files via upload

--- jogo de adivinhação em python/AdivinhaçãoemPython.py
+++ jogo de adivinhação em python/AdivinhaçãoemPython.py
@@ -1,9 +1,20 @@
 print("********************************")
-print("Bem vindo no jogo de Adivinhação")
+print("Bem vindo no jogo de Adivinhação!")
 print("********************************")
 
 numero_secreto = 42
 
-chute = input("Digite o seu numero: ")
+chute_str = input("Digite o seu numero: ")
 
-print("Você digitou" , chute)
+print("Você digitou" , chute_str)
+
+chute = int(chute_str)
+
+if(numero_secreto == chute ):
+    print("Você acertou!")
+else: 
+    if(chute > numero_secreto):
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    if(chute < numero_secreto):
+        print("Você errou! O seu chute foi menor do que o número secreto.")
+print("Fim do jogo")
